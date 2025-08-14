79f854588680f790d9ffc7585b7b35dd488af562
Harun Ardiansyah <harunardi13@gmail.com>
2025-08-14 10:09:58 -0800

    add `pytest` and `doctest`

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numbers
+import doctest
 
 def test_hypot():
     from mygeopy.triangle import hypot
@@ -7,3 +8,6 @@ def test_hypot():
     assert hypot(3, 4) == 5
     assert hypot(5, 12) == 13
     assert hypot(-8, 15) == 17
+
+# run pytest -> `pytest` in command line
+# run doctest -> `pytest --doctest-modules` in command line
